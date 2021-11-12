9f3b8078da7ed1f75aff91ea6f3e5c11edde74e1
pserb <paulserbanescu3@gmail.com>
2021-11-11 22:52:17 -0500

    create navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import navbar from '../styles/Navbar.module.scss'
+
+export default function Navbar({ }) {
+    return <>
+        <nav className={navbar.nav}>
+            <Link href="/"><h1 className={navbar.title}><span className={navbar.subtitle}>personal homepage</span>Paul Serbanescu</h1></Link>
+
+            <Link href="/portfolio"><span className={navbar.link}>portfolio</span></Link>
+        </nav>
+    </>
+}
