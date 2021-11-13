6f76aa32500774f48f4d2f3a1bddcbf18a2640d3
pserb <paulserbanescu3@gmail.com>
2021-11-12 21:23:50 -0500

    update navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,6 +5,7 @@ export default function Navbar({ }) {
     return <>
         <nav className={navbar.nav}>
             <Link href="/"><h1 className={navbar.title}><span className={navbar.subtitle}>personal homepage</span>Paul Serbanescu</h1></Link>
+            <Link href="/"><span className={navbar.titledesc}>&uarr; click to go home</span></Link>
 
             <Link href="/portfolio"><span className={navbar.link}>portfolio</span></Link>
         </nav>
