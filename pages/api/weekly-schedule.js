10e6c0f8d3c94df0c049c5ffcd4dd0a03f5fa4eb
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-06-20 02:03:03 -0400

    updt

--- pages/api/weekly-schedule.js
+++ pages/api/weekly-schedule.js
@@ -14,39 +14,39 @@ export default function handler(req, res) {
       "scheduleType": "week",
       "days": [
         {
-          "day": "June 13, 2022",
-          "bell": "regular_schedule",
-          "block": "B",
-          "testing": "Makeup Test Day",
-          "announcement": null
-        },
-        {
-          "day": "June 14, 2022",
-          "bell": "homeroom_schedule",
-          "block": "A",
-          "testing": "Makeup Test Day",
-          "announcement": "Special 20 - Minute Homeroom Bell Schedule. End of Marking Period 3. (Note: app may not display correct times)."
-        },
-        {
-          "day": "June 15, 2022",
+          "day": "June 20, 2022",
           "bell": "NO SCHOOL",
           "block": null,
           "testing": "Regents Exams",
-          "announcement": "English Language Arts (9:15 AM) and Living Environment (1:15 PM) regents testing today."
+          "announcement": "Juneteenth. No Regents Exams."
         },
         {
-          "day": "June 16, 2022",
+          "day": "June 21, 2022",
           "bell": "NO SCHOOL",
           "block": null,
           "testing": "Regents Exams",
-          "announcement": "Algebra I (9:15 AM) and Physical Setting / Chemistry (1:15 PM) regents testing today."
+          "announcement": "Geometry (8:15 AM) and World Language (8:15 or 12:15 PM) regents testing today."
         },
         {
-          "day": "June 17, 2022",
+          "day": "June 22, 2022",
           "bell": "NO SCHOOL",
           "block": null,
           "testing": "Regents Exams",
-          "announcement": "Global History and Geography II (9:15 AM) and Physical Setting / Earth Science (1:15 PM) regents testing today."
+          "announcement": "Algebra II (8:15 AM) regents testing today."
+        },
+        {
+          "day": "June 23, 2022",
+          "bell": "NO SCHOOL",
+          "block": null,
+          "testing": "Regents Exams",
+          "announcement": "Physical Setting / Physics (8:15 AM) regents testing today. Senior Graduation!"
+        },
+        {
+          "day": "June 24, 2022",
+          "bell": "NO SCHOOL",
+          "block": null,
+          "testing": "Regents Exams",
+          "announcement": "Teacher grades due 8 AM."
         }
       ]
     }
