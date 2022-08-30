3df239fbda014f0a8d3ab2c0eb4582d19a97f42a
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-08-30 18:04:45 -0400

    updt

--- components/Block.js
+++ components/Block.js
@@ -1,18 +1,7 @@
-import block from '../styles/Block.module.scss';
+import block from '../styles/Block.module.scss'
 import Link from 'next/link'
 
-export default function Block({ children, title, smallButton, smallButtonLink, mediumButton, mediumButtonLink, buttonList }) {
-    
-    function hasButton() {
-        if (smallButton != null) {
-            return <Link href={smallButtonLink}><button className={block.buttonsm}>{smallButton}</button></Link>
-        } else if (mediumButton != null) {
-            return <Link href={mediumButtonLink}><button className={block.buttonmd}>{mediumButton}</button></Link>
-        } else if (buttonList != null) {
-            return <> { buttonList.map((x, i) => <button className={block.buttonlist} key={i}>{x}</button> )} </>
-        }
-    };
-
+export default function Block({ children, title }) {
     return (
         <div className={block.content}>
             <div className={block.section}>
@@ -20,7 +9,6 @@ export default function Block({ children, title, smallButton, smallButtonLink, m
                 <p className={block.sectioncontent}>
                     {children}
                 </p>
-                {hasButton()}
             </div>
         </div>
     )
