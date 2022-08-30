226273c446ecbb13b7769c3c9b3d3dd1ec49af98
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-08-30 18:04:38 -0400

    favicon

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,12 @@
+import Head from 'next/head'
 import Navbar from './Navbar'
 
 export default function Layout({ children }) {
     return <>
+        <Head>
+            <link rel='icon' href="/img/logo.png"></link>
+        </Head>
+
         <Navbar />
         {children}
     </>
