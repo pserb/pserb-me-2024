081f0927bcc8ce71743066308cc5f348320873fd
Paul Serbanescu <paulserbanescu3@gmail.com>
2023-07-28 18:36:13 -0400

    updt

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
         </div>
 
         <div className={home.text}>
-          <p>Hello! I&apos;m a senior at Stuyvesant High school in New York City.<br/><br/>
+          <p>Hello! I&apos;m a freshman at Binghamton University in New York.<br/><br/>
           I am passionate about robotics, app development, computer programming, web development and design, and 3D modeling and rendering.</p>
           <p></p>
           <Link href="/portfolio"><button className={home.button}>Portfolio</button></Link>
