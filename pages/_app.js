9dd907dbc9dd20fa17525d3a80131037035c86ec
Paul Serbanescu <paulserbanescu3@gmail.com>
2023-10-10 16:06:20 -0400

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
+import { Analytics } from '@vercel/analytics/react'
+
 import '../styles/globals.scss'
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Analytics />
       <Component {...pageProps} />
     </Layout>
   )
